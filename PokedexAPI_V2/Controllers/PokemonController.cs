6ac3aa06db3a1782747899879180761265275d15
Hitsu91 <lucafang@hotmail.it>
2021-10-12 12:52:50 +0200

    Aggiungere i file di progetto.

---
+++ PokedexAPI_V2/Controllers/PokemonController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PokedexAPI_V2.Models;
+using PokedexAPI_V2.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PokedexAPI_V2.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PokemonController : ControllerBase
+    {
+        private readonly IPokemonService _pokemonService;
+
+        public PokemonController(IPokemonService pokemonService)
+        {
+            _pokemonService = pokemonService;
+        }
+
+        [HttpGet]
+        public List<Pokemon> GetAll()
+        {
+            return _pokemonService.GetAll();
+        }
+    }
+}
